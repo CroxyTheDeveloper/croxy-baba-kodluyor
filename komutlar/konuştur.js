9543d2c49c3e87ac943635218d62cb14ac49229d
Burak <43144955+CroxyTheDeveloper@users.noreply.github.com>
2018-09-10 19:19:14 +0300

    Create konuştur.js

---
+++ komutlar/konuştur.js
@@ -0,0 +1,13 @@
+//Konuştur komudu ile, botu konuşturabilirsiniz.//
+
+const Discord = require('discord.js') //Bizim için en önemli olan, discord.js modülünü indiriyoruz.
+
+module.exports.run = async (bot, message, args) => { //Kodlarımızı gireceğimiz yeri ayarlıyoruz.
+	const yazi = args.join(" ") //Yazımızı, kullanıcının bota yazdırmasını istediği şey olarak ayarlıyoruz.
+	if(!yazi) return message.channel.send('Ne yazdırmak istersin koçum?') //Eğer kullanıcı sadece komudu girip, bir şey yazmadıysa bu yazıyı göndersin.
+	message.channel.send(yazi) //Yazıyı gönderiyoruz.
+}//Komudumuzu bitiriyoruz.
+
+module.exports.help = { //Komudumuzu kullanmamıza yarayan yerleri giriyoruz.
+	name: 'konuştur' //Komudumuzun ismini giriyoruz.
+}//Ve komudumuzu bitiriyoruz.
